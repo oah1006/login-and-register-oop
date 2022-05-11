4aec54ddd2987010d499f5fc392ac24b93219ff6
OaH <81689054+oah1006@users.noreply.github.com>
2022-05-11 20:57:23 +0700

    OOP: create set users function Register class

--- classes/Register.php
+++ classes/Register.php
@@ -1,6 +1,19 @@
 <?php
 
 class Register extends DB {
+    protected function setUsers($username, $email) {
+        $sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?);";
+        $stmt = $this->connect()->prepare($sql);
+
+        $hashingPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        if ($stmt->execute(array($username, $password, $email))) {
+            $stmt = null;
+            header("Location: ../index.php?error=stmtfailed");
+            exit();
+        }
+    }
+
     protected function checkUsers($username, $email) {
         $sql = "SELECT * FROM users WHERE username = ? or email = ?;";
         $stmt = $this->connect()->prepare($sql);
