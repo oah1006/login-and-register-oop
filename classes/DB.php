8f69f9ead1a56350a260f74656475102de1955f1
OaH <81689054+oah1006@users.noreply.github.com>
2022-05-11 20:25:13 +0700

    OPP: instantiate connect DB class

---
+++ classes/DB.php
@@ -0,0 +1,25 @@
+<?php
+
+class DB {
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $db = "login";
+    
+    private function connect() {
+        try {
+            $pdo = new PDO("mysql:host=" . $this->host . ";dbname=" . this->db, $username, $password);
+            return $dbo;
+        } catch (PDOException $e) {
+            print("Error! " . $e->getMessage() . "<br/>");
+            die();
+        }
+    }
+}
+
+
+
+
+
+
+?>
